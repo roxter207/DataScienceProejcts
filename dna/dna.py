172accd5e91698576eb2adf5e4255bfe98bf56cf
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-09-03 20:05:01 +0530

    dna1

--- dna/dna.py
+++ dna/dna.py
@@ -8,4 +8,15 @@ st.write("""DNA Nucleotide Count Web App""")
 
 st.header("INPUT (DNA QUERY)")
 
-sequence_input = ">DNA Query\nGAAACA"
+sequence_input = ">DNA Query\nGAAACATCTGAAATGCATAGGCAAAGTAAGTTGATGATCTATCTGACCTGACTGATCGCGTAGA"
+sequence = st.text_area("SEQUENCE input", sequence_input, height=25)
+sequence = sequence.splitlines()
+sequence = sequence[1:]
+sequence = ''.join(sequence)
+
+
+st.warning("""****""")
+
+st.header('INPUT (DNA QUERY)')
+st.header("OUTPUT DNA COUNT")
+
