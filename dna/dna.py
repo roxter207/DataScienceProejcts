9ad6b78202fdd6d11c2c3d25f1b3f088601469b7
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-09-03 20:00:05 +0530

    dna

---
+++ dna/dna.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+
+st.write("""DNA Nucleotide Count Web App""")
+
+
+st.header("INPUT (DNA QUERY)")
+
+sequence_input = ">DNA Query\nGAAACA"
